f2c104a0bee283f901141985c1a22a444e59860a
Lucky <tutrinh3004@gmail.com>
2017-10-19 23:53:30 +0700

    Class inheritance

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-console.log('Hello from my ass');
+import Person from './person';
+
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  greet() {
+    console.log(`Hello is it ${this.name} you're looking for?`);
+  }
+}
+
+let nick = new Student('Nick', 30);
+nick.greet();
